8785c198639a0f4cbaf612de7d6d7f5f4b9af66a
jaminhoood <princeobj5@gmail.com>
2023-11-13 15:52:30 +0100

    first commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
-export const API_URL = `http://localhost/dev/wp-json/l-card/v1`
+// export const API_URL = `http://localhost/dev/wp-json/l-card/v1`
+export const API_URL = `https://myluxtrade.com/dev/wp-json/l-card/v1`
